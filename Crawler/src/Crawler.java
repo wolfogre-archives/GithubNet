ddf557b060d2d28abd61fc5735e74f59b146d625
Jason Song <wolfogre@outlook.com>
2016-01-23 00:15:47 +0800

    JSoup is cute, 429 is't cute

--- Crawler/src/Crawler.java
+++ Crawler/src/Crawler.java
@@ -1,5 +1,7 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.sql.*;
@@ -37,22 +39,41 @@ public class Crawler {
 	}
 
 	String[] getFollowers(String username){
+		int sleepTime = 0;
 		List followers = new LinkedList<>();
+		int page = 1;
 		while(true){
 			try {
-				Document document = Jsoup.connect("https://github.com/wolfogre/followers").get();
-				String html = document.html();
-				followers.add(html);
+				Document document = Jsoup.connect("https://github.com/" + username + "/followers?page=" + page).get();
+				Elements links = document.select("h3[class=follow-list-name]");
+				if(links.isEmpty())
+					break;
+				for (Element link : links) {
+					Document doc = Jsoup.parse(link.html());
+					Element name = doc.select("a[href]").first();
+					followers.add(name.attr("href"));
+				}
 			} catch (IOException e) {
+				--page;
+				System.out.println(e.getMessage());
 				e.printStackTrace();
+				try {
+					sleepTime += 60000;
+					Thread.sleep(60000);
+					System.out.println("sleepTime  " + sleepTime);
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
 			}
-			break;
+			System.out.println(followers.size() + "  " + page);
+			++page;
 		}
 
 		String[] result = new String[followers.size()];
 		for(int i = 0; i < followers.size(); ++i){
-			result[i] = (String)followers.get(i);
+			result[i] = ((String)followers.get(i)).replace("//", "");
 		}
+		System.out.println("sleepTime  " + sleepTime);
 		return result;
 	}
 }
--- Crawler/src/DebugCrawler.java
+++ Crawler/src/DebugCrawler.java
@@ -11,12 +11,21 @@ public class DebugCrawler {
 	public static void main(String args[]){
 
 		Crawler crawler = new Crawler();
+		/*
 		try {
 			crawler.connectDatabase("com.mysql.jdbc.Driver", "jdbc:mysql://120.27.99.15:3306/github_net", "wolf", new Scanner(System.in).nextLine());
 		} catch (SQLException | ClassNotFoundException | IllegalAccessException | InstantiationException e) {
 			e.printStackTrace();
 		}
-		String[] result = crawler.getFollowers("wolfogre");
-		System.out.print(result[0]);
+		*/
+		String[] result;
+		for(int i = 0; i < 10; ++i)
+			result = crawler.getFollowers("PresidentObamaBot");
+
+		//for (String str : result) {
+			//System.out.println(str);
+		//}
+		//System.out.println(result.length);
+
 	}
 }
